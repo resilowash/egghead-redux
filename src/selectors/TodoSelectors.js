191cf1fc41c31d47f722ae1fd8120f6a5709a68d
Ryan Silowash <rsilowash@iq-inc.com>
2018-01-30 18:04:37 -0500

    fixed the state to use the root reducer, still broken then

--- src/selectors/TodoSelectors.js
+++ src/selectors/TodoSelectors.js
@@ -7,8 +7,8 @@ import { ADD_NEW } from '../actions/TodoActions';
 //export const getTodoItems = (state) => todoSelector.getTodoItems(state);
 
 //map the function here to return the part of state that we want
-const getTodoItemsFromState = (state) => state.todoList;
-const getEntireTodoFromState = (state) => state;
+const getTodoItemsFromState = (state) => state.todos.todoList;
+const getEntireTodoFromState = (state) => state.todos;
 
 //create the selector -- don't know much about why this is how this works yet... todo find out.
 export const getTodoListItems = createSelector(
